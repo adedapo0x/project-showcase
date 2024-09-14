a8d48fb63a2a7ef6597ea43542d9e644b5a4aeb3
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-14 21:28:26 +0100

    Sequelize CLI installed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-
+const authRouter = require('./routes/auth.routes')
 
 const app = express()
 
-a
+app.use('/api/v1/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Server up and running")
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,3 +2,6 @@ const router = require('express').Router()
 const signUp = require('../controllers/auth.controller')
 
 router.post('/sign-up', signUp)
+
+
+module.exports = signUp
