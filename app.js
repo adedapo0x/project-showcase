97622ee7f84e8cf0c3b385dbed99a0caa2ce4f0d
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-15 10:19:44 +0100

    DB file and its migration added to codebase

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express')
 const authRouter = require('./routes/auth.routes')
 
 const app = express()
+app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
 
+app.use('/*', (req, res, next) => {
+    return res.status(404).json({message: "Page Not Found!"})
+})
 app.listen(process.env.PORT, () => {
     console.log("Server up and running")
 })
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,27 @@
+const user = require('../db/models/user')
 const signUp = async (req, res) =>{
     try{
-        res.json({message: "Sign up api working, lol"})
+        const { userType, firstName, lastName, email, password, confirm_pw } = req.body
+
+        const existingUser = await user.findOne({ where: { email }})
+        if (existingUser){
+            return res.status(400).json({message: "Email already in use. Try another email!"})
+        }
+
+        if (!['0', '1'].includes(userType)){
+            return res.status(400).json({
+                status: 'failed',
+                message: "Invalid user type for sign up"
+            })
+        }
+
+        const newUser = await user.create({
+            userType, firstName, lastName, email, password
+        })
+
+        if (!newUser){
+            return res.status(400).json({message: 'Invalid user credentials'})
+        }
     } catch{
 
     }
