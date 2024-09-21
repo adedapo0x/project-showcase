63c5cb7250a6eb508c01b956f0bdaa5f292173c4
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-21 14:38:58 +0100

    added authenticateToken functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,30 +1,32 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const user = require('../db/models/user')
+const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
 const generateAccessToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN})
 }
 
-const signUp = async (req, res) =>{
-    try{
+const signUp = catchAsync(async (req, res, next) =>{
         const { userType, firstName, lastName, email, password, confirmPassword } = req.body
 
         const existingUser = await user.findOne({ where: { email }})
         if (existingUser){
-            return res.status(400).json({message: "Email already in use. Try another email!"})
+            return next(new AppError('Email already in use. Try another email!', 400))
         }
 
         if (!['0', '1'].includes(userType)){
-            return res.status(400).json({
-                status: 'failed',
-                message: "Invalid user type for sign up"
-            })
+            throw new AppError('Invalid user type', 400)
         }
 
         const newUser = await user.create({
             userType, firstName, lastName, email, password, confirmPassword
          })
+
+        if (!newUser){
+            return next(new AppError('Invalid user credentials', 400))
+        }
 
         const result = newUser.toJSON()
         delete result.password
@@ -32,38 +34,41 @@ const signUp = async (req, res) =>{
 
         result.token = generateAccessToken({ authId: result.id })
 
-        if (!result){
-            return res.status(400).json({message: 'Invalid user credentials'})
-        }
-
         return res.status(201).json({status: 'success',
         message: "User created successfully!", data: result})
-    } catch(err){
-        console.log(err)
-        res.status(400).json({message: "Error encountered. Please check all inputted details and try again"})
-    }
-}
+})
 
 
-const logIn = async (req, res) => {
-    try{
+const logIn = catchAsync(async (req, res, next) => {
         const { email, password } = req.body
         if (!email || !password) {
-            return res.status(400).json({status: "error",
-            message: "Please provide email and password"})
+            return next(new AppError("Please provide email and password", 400))
         }
 
         const result = await user.findOne({ where: { email }})
         if (!result || !(await bcrypt.compare(password, result.password) )){
-            return res.status(401).json({status: 'failed',
-            message: "Incorrect email or password!"})
+            return next(new AppError("Incorrect email or password!", 401))
         }
 
         const token = generateAccessToken({ authId: result.id })
         return res.json({status: "success", token })
-    } catch{
-        console.log(e)
-        return res.status(400).json({message: "Error encountered, login not successful!"})
+})
+
+const authenticateToken = catchAsync(async (req, res, next) => {
+    const token = ''
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        const token = req.headers.authorization.split(' ')[1]
+    } else {
+        return next(AppError('Unauthorized! Please login to get access', 401))
     }
-}
-module.exports = { signUp, logIn }
+
+    const loggedInUserObj = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    if (!loggedInUserObj){
+        return next(new AppError('User does not exist!', 400))
+    }
+
+    req.user = await user.findByPk(loggedInUserObj.authId)
+    next()
+})
+
+module.exports = { signUp, logIn, authenticateToken }
