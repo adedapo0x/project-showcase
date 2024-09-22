a4de53615e301e0c2fd9157918edb36d7fa371ce
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-22 02:14:01 +0100

    added project routes to application

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const express = require('express')
 const authRouter = require('./routes/auth.routes')
+const projectRouter = require('./routes/project.route')
 const { connectToDatabase } = require('./config/database')
+const catchAsync = require('./utils/catchAsync')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/error.controller')
 
 const app = express()
+
 app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/projects', projectRouter)
 
-app.use('/*', (req, res, next) => {
-    return res.status(404).json({message: "Page Not Found!"})
-})
+app.use('/*', catchAsync(async (req, res, next) => {
+    throw new AppError('Route is not found', 404)
+}))
+
+app.use(globalErrorHandler)
 
 const startServer = async () =>{
     try{
